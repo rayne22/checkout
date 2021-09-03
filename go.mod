f9fc5d9bc1680631fac212b477ff24d856f06226
Changa Lesa <changalesa8@gmail.com>
2021-09-03 19:09:46 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module pareza-st
+
+go 1.15
+
+require github.com/stripe/stripe-go/v72 v72.64.1 // indirect
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/checkout/session"
+)
+
+func main() {
+	// This is your real test secret API key.
+	stripe.Key = "sk_test_51G7PgLF8tXEBf9tJWq7hFIjcgndXfCNvg8LvdzQhYO5owePRIablydBvB2GMX4TfP2aDPyXEQidIOmqjOCiUByUr00MXBqqrcb"
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/create-checkout-session", createCheckoutSession)
+	addr := "localhost:4242"
+	log.Printf("Listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func createCheckoutSession(w http.ResponseWriter, r *http.Request) {
+	domain := "http://localhost:4242"
+	params := &stripe.CheckoutSessionParams{
+		LineItems: []*stripe.CheckoutSessionLineItemParams{
+			&stripe.CheckoutSessionLineItemParams{
+				// TODO: replace this with the `price` of the product you want to sell
+				Price: stripe.String("{{PRICE_ID}}"),
+				Quantity: stripe.Int64(1),
+			},
+		},
+		PaymentMethodTypes: stripe.StringSlice([]string{
+			"sepa_debit",
+		}),
+		Mode: stripe.String(string(stripe.CheckoutSessionModePayment)),
+		SuccessURL: stripe.String(domain + "/success.html"),
+		CancelURL: stripe.String(domain + "/cancel.html"),
+	}
+
+	s, err := session.New(params)
+
+	if err != nil {
+		log.Printf("session.New: %v", err)
+	}
+
+	http.Redirect(w, r, s.URL, http.StatusSeeOther)
+}
